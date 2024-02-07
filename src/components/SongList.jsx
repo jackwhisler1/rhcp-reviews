85fe8a6167b6a59831af8d1490fbaa8b487e09c6
Jack Whisler <78163023+jackwhisler1@users.noreply.github.com>
2024-02-06 21:58:39 -0600

    fix

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
-import Song from './Song';
 import '../styling/Styles.css' 
 import BaseContainer from './Container';
 import StadiumArcadium from './stadium-arcadium-rankings.json';
