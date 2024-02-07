983738ac384c5850fba995cd02034dcd33572cdc
Jack Whisler <78163023+jackwhisler1@users.noreply.github.com>
2024-02-06 22:57:46 -0600

    need to fix json rankings

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import '../styling/Styles.css' 
 import BaseContainer from './Container';
 import StadiumArcadium from './stadium-arcadium-rankings.json';
+import StadiumArcadiumArtwork from '../styling/stadium-album-art.svg';
 const SongList = () => {
   const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
   const [songs, setSongs] = useState([]);
@@ -45,26 +46,27 @@ const SongList = () => {
       const sum = reviews.reduce((total, review) => total + review.rating, 0);
       return sum / reviews.length;
     };
-  
-    const songsWithAverageRating = StadiumArcadium.songs.map(song => ({
+
+    const songsWithAverageRating = StadiumArcadium.songs.map((song) => ({
       ...song,
       averageRating: calculateAverageRating(song.reviews),
     }));
-  
-    setSongs(songsWithAverageRating);
+
+    const sortedSongsByTrackNumber = [...songsWithAverageRating].sort((b, a) => a.trackNumber - b.trackNumber);
+
+    setSongs(sortedSongsByTrackNumber);
   }, []);
-  
 
   return (
     <BaseContainer>
-      <div className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200 h-full overflow-y-auto">
-        <header className="px-5 py-4 border-b border-gray-100">
-          <h2 className="font-semibold text-red-700">Red Hot Takes</h2>
+      <div className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border-rounded-sm border-gray-200 h-full overflow-y-auto">
+        <header className="px-5 py-4 border-b border-gray-100 sticky top-0 z-50 bg-white text-xl">
+          <h2 className="text-s font-semibold text-red-700">Red Hot Takes</h2>
         </header>
         <div className="p-3">
-          <div className="overflow-x-auto">
-            <table className="table-auto w-full" style={{ caretColor: 'transparent' }}>
-              <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+          <div className="">
+            <table className="table-auto w-full " style={{ caretColor: 'transparent' }}>
+              <thead className="text-s font-semibold uppercase text-gray-400 bg-gray-50 initial z-40">
                 <tr>
                   <th className="p-2 whitespace-nowrap cursor-pointer text-red-700" onClick={() => handleSort('songName')}>
                     <div className="font-semibold text-left">Song</div>
@@ -81,7 +83,9 @@ const SongList = () => {
                 {sortedSongs.map(song => (
                   <tr key={song.id}>
                     <td className="p-2 whitespace-nowrap">{song.songName}</td>
-                    <td className="p-2 whitespace-nowrap">{song.albumName}</td>
+                    <td className="p-2">                      
+                      <img src={StadiumArcadiumArtwork} alt="Album Artwork" className="w-8 h-8" />
+                    </td>
                     <td className="p-2 whitespace-nowrap">{song.averageRating.toFixed(2)}</td>
                   </tr>
                 ))}
