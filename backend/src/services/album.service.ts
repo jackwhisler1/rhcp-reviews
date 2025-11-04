c89fd026691be6a3ac88084b7dae6042a16fd3bc
Jack Whisler <78163023+jackwhisler1@users.noreply.github.com>
2025-11-04 15:15:56 -0600

    jw update

--- backend/src/services/album.service.ts
+++ backend/src/services/album.service.ts
@@ -82,7 +82,7 @@ export const getAlbumSongStatsService = async ({
       select: { songId: true, rating: true, id: true },
       orderBy: { createdAt: "desc" },
     });
-    console.log("selectedUserReviews:", selectedUserReviews);
+    console.log("selectedUserReviews:");
   }
 
   // Merge data
